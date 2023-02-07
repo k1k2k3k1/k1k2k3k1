2734081a4752f075c1cf584a5546931ead1ded25
khushbu singh <114380485+k1k2k3k1@users.noreply.github.com>
2023-02-07 18:31:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import "../styles/App.css";
+import Swap from "./Swap";
+import Source from "./Source";
+import Destination from "./Destination";
+import { Date } from "./Date";
+import SearchBus from "./SearchBus";
+import Nav from "./Nav";
+import { Image } from "./Image";
+import { BusDetails } from "./BusDetails";
+import { BusCard } from "./BusCard";
+import { Booked } from "./Booked";
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  const [message, setMessage] = useState("");
+  const [toMessage, setToMessage] = useState("");
+  const [select, setSelect] = useState("");
+  const [data, setData] = useState([]);
+  const [data2, setData2] = useState([]);
+  const [filter, setFilter] = useState([]);
+  const [show, setShow] = useState({});
+
+  return (
+    <div id="main">
+      <Nav />
+
+      <div id="main-duplicate">
+        <Source message={message} setMessage={setMessage} />
+        <Swap
+          source={message}
+          destination={toMessage}
+          setSource={setMessage}
+          setToSource={setToMessage}
+        />
+        <Destination toMessage={toMessage} setToMessage={setToMessage} />
+        <Date select={select} setSelect={setSelect} />
+        <SearchBus
+          from={message}
+          to={toMessage}
+          date={select}
+          data={data}
+          data2={data2}
+          filter={filter}
+          setData={setData}
+          setData2={setData2}
+          setFilter={setFilter}
+        />
+      </div>
+      <Routes>
+        <Route path="/" element={<Image />} />
+        <Route
+          path="BusDetails"
+          element={
+            <BusDetails
+              filter={filter}
+              setShow={setShow}
+              data={data}
+              data2={data2}
+            />
+          }
+        />
+        <Route path="BusDetails/BusCard" element={<BusCard show={show} />} />
+        <Route path="BusDetails/BusCard/Booked" element={<Booked />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
